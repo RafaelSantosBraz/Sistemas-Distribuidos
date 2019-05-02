0eea61e0f21fc751ec193f412e7f0215a9586dcf
José Henrique Ricordi Cruz <cc45966446830@L3C15X.lab.cct.uenp.edu.br>
2019-05-02 14:51:59 -0300

    Inicio Interface

--- Banco 24 Horas RMI/CaixaEletronico/src/controler/Run.java
+++ Banco 24 Horas RMI/CaixaEletronico/src/controler/Run.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package caixaeletronico;
+package controler;
 
 import classses.Cliente;
 import com.google.gson.Gson;
