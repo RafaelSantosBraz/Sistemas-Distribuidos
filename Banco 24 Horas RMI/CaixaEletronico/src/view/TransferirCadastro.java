24d25bce2ffb93fadb0ba4c3b7587dd1a824522b
Henrique Ricordi <henriquericordi@gmail.com>
2019-05-04 20:10:34 +0000

    Coreção

--- Banco 24 Horas RMI/CaixaEletronico/src/view/TransferirCadastro.java
+++ Banco 24 Horas RMI/CaixaEletronico/src/view/TransferirCadastro.java
@@ -165,7 +165,6 @@ public class TransferirCadastro extends javax.swing.JFrame {
 
     private void jComboBox1ItemStateChanged(java.awt.event.ItemEvent evt) {//GEN-FIRST:event_jComboBox1ItemStateChanged
         // TODO add your handling code here:
-        Controladora.getInstancia().setBanco(jComboBox1.getSelectedItem().toString());
     }//GEN-LAST:event_jComboBox1ItemStateChanged
 
     private void jComboBox1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jComboBox1ActionPerformed
