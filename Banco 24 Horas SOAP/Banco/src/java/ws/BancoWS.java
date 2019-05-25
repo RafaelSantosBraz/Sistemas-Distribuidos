68f9f81b741a4f929ea8fed5b466824283f6bc09
rafaelsantosbraz <@rafaelsantosbraz>
2019-05-24 22:21:56 -0300

    Criação de todos os web métodos no BancoWS

--- Banco 24 Horas SOAP/Banco/src/java/ws/BancoWS.java
+++ Banco 24 Horas SOAP/Banco/src/java/ws/BancoWS.java
@@ -2,6 +2,7 @@ package ws;
 
 import classses.*;
 import controller.*;
+import java.util.ArrayList;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.WebParam;
@@ -21,11 +22,6 @@ public class BancoWS {
         controller = new ControllerBanco("localhost", "3306", "banco", "mysql", "mysql");
     }
 
-    @WebMethod(operationName = "hello")
-    public String hello(@WebParam(name = "name") String txt) {
-        return "Hello " + txt + " !";
-    }
-
     @WebMethod(operationName = "consultarCadastro")
     public Cliente consultarCadastro(@WebParam(name = "CPF") String CPF) {
         return controller.consultarCadastro(CPF);
@@ -34,5 +30,45 @@ public class BancoWS {
     @WebMethod(operationName = "alterarCadastro")
     public boolean alterarCadastro(@WebParam(name = "cliente") Cliente cliente) {
         return controller.alterarCadastro(cliente);
+    }
+
+    @WebMethod(operationName = "realizarSaque")
+    public boolean realizarSaque(@WebParam(name = "conta") int conta, @WebParam(name = "valor") double valor) {
+        return controller.realizarSaque(conta, valor);
+    }
+
+    @WebMethod(operationName = "realizarDeposito")
+    public boolean realizarDeposito(@WebParam(name = "conta") int conta, @WebParam(name = "valor") double valor) {
+        return controller.realizarDeposito(conta, valor);
+    }
+
+    @WebMethod(operationName = "consultarSaldo")
+    public double consultarSaldo(@WebParam(name = "conta") int conta) {
+        return controller.consultarSaldo(conta);
+    }
+
+    @WebMethod(operationName = "realizarTransferencia")
+    public boolean realizarTransferencia(@WebParam(name = "contaOrigem") int contaOrigem, @WebParam(name = "contaDestino") int contaDestino, @WebParam(name = "valor") double valor) {
+        return controller.realizarTransferencia(contaOrigem, contaDestino, valor);
+    }
+
+    @WebMethod(operationName = "consultarExtrato")
+    public ArrayList<Operacao> consultarExtrato(@WebParam(name = "conta") int conta) {
+        return controller.consultarExtrato(conta);
+    }
+
+    @WebMethod(operationName = "criarCadastro")
+    public boolean criarCadastro(@WebParam(name = "cliente") Cliente cliente) {
+        return controller.criarCadastro(cliente);
+    }
+
+    @WebMethod(operationName = "criarConta")
+    public boolean criarConta(@WebParam(name = "CPF") String CPF, @WebParam(name = "saldo") double saldo) {
+        return controller.criarConta(CPF, saldo);
+    }
+
+    @WebMethod(operationName = "consultarNumerosContasCliente")
+    public ArrayList<Integer> consultarNumerosContasCliente(@WebParam(name = "CPF") String CPF) {
+        return controller.consultarNumerosContasCliente(CPF);
     }
 }
