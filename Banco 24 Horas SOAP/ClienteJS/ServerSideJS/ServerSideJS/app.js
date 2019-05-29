dfd8061b0b7b232253848d8a02711ff6782888af
rafaelsantosbraz <@rafaelsantosbraz>
2019-05-29 19:03:23 -0300

    Continuação server/cliente JS - ainda em testes de conexão e resposta

--- Banco 24 Horas SOAP/ClienteJS/ServerSideJS/ServerSideJS/app.js
+++ Banco 24 Horas SOAP/ClienteJS/ServerSideJS/ServerSideJS/app.js
@@ -1,65 +1,33 @@
-﻿'use strict';
-var debug = require('debug');
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
+﻿var express = require('express');
+var app = express();
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+// Métodos do servidor JS (Node.JS) para o Cliente JS (HTML + JavaScript)
+app.post('/consultar_cadastro', urlencodedParser, function (req, res) {
+    console.log(req);
+    // Prepare output in JSON format    
+    response = {
+        CPF: req.body.CPF,
+        nome: "Rafael"
+    }; 
+    var htmlCorpo = '<form id="formulario" action="http://localhost:7000/consultar_cadastro" method="POST"> CPF: <input type = "text" name = "CPF"> <br> Nome: <input type = "text" name = "nome"> <button>consultar</button> </form>';
+    res.end(htmlCabecalho + htmlCorpo + htmlFim);
+})
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// Inicialização do servidor JS
+var server = app.listen(7000, function () {
+    var host = "localhost";
+    var port = 7000;
+    console.log("Example app listening at http://%s:%s", host, port);
+})
 
-app.use('/', routes);
-app.use('/users', users);
+// Variáveis HTML 
+var htmlCabecalho = '<!DOCTYPE html> <html> <head> <title>Cliente JS</title> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script> </head> <body>';
+var htmlFim = '</body></html >';
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+// Métodos para requisições SOAP com o Banco
 
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function (err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-app.set('port', process.env.PORT || 3000);
-
-var server = app.listen(app.get('port'), function () {
-    debug('Express server listening on port ' + server.address().port);
-});
