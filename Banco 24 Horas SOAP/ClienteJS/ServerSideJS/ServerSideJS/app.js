ad6cc4da8edea9191a97d0784c6878333f43d6ee
rafaelsantosbraz <@rafaelsantosbraz>
2019-05-30 00:34:37 -0300

    Adição de SOAP ao servidor JS - testes em andamento

--- Banco 24 Horas SOAP/ClienteJS/ServerSideJS/ServerSideJS/app.js
+++ Banco 24 Horas SOAP/ClienteJS/ServerSideJS/ServerSideJS/app.js
@@ -1,6 +1,9 @@
-﻿var express = require('express');
+﻿// Variáveis do Node.js e SOAP
+var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var soap = require('soap');
+var url = 'http://localhost:8080/BancoWS/BancoWS?WSDL';
 
 // Create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
@@ -8,14 +11,12 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // Métodos do servidor JS (Node.JS) para o Cliente JS (HTML + JavaScript)
 app.post('/consultar_cadastro', urlencodedParser, function (req, res) {
-    console.log(req);
-    // Prepare output in JSON format    
+    //console.log(req);    
     response = {
         CPF: req.body.CPF,
         nome: "Rafael"
-    }; 
-    var htmlCorpo = '<form id="formulario" action="http://localhost:7000/consultar_cadastro" method="POST"> CPF: <input type = "text" name = "CPF"> <br> Nome: <input type = "text" name = "nome"> <button>consultar</button> </form>';
-    res.end(htmlCabecalho + htmlCorpo + htmlFim);
+    };     
+    consultarCadastro({ CPF: "47192962899" }, res);
 })
 
 // Inicialização do servidor JS
@@ -28,6 +29,15 @@ var server = app.listen(7000, function () {
 // Variáveis HTML 
 var htmlCabecalho = '<!DOCTYPE html> <html> <head> <title>Cliente JS</title> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script> </head> <body>';
 var htmlFim = '</body></html >';
+var htmlCorpo = '';
 
 // Métodos para requisições SOAP com o Banco
-
+function consultarCadastro(argsRequisicao, response) {
+    soap.createClient(url, function (err, client) {
+        client.consultarCadastro(argsRequisicao, function (err, result) {
+            console.log(result);
+            htmlCorpo = '<form id="formulario" action="http://localhost:7000/consultar_cadastro" method="POST"> CPF: <input type = "text" name = "CPF"> <br> Nome: <input type = "text" name = "nome"> <button>consultar</button> </form>';
+            response.end(htmlCabecalho + htmlCorpo + htmlFim);
+        });
+    });
+}
